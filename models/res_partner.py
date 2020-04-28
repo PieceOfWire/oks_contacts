98479db003f3deaf573781f29e115293de571f2e
CrazyBolillo <random@email.com>
2020-04-28 16:23:11 -0500

    Override name_get method No need to change views to display the correct name. Just changed one line from the name_get method.

--- models/res_partner.py
+++ models/res_partner.py
@@ -19,3 +19,29 @@ class OksContact(models.Model):
                     partner.display_name = partner.x_commercial_name
             else:
                 partner.display_name = partner.name
+
+    # Overriden method.
+    @api.multi
+    def name_get(self):
+        res = []
+        for partner in self:
+            # Only change. Originally it was: name = partner.name
+            name = partner.display_name or ''
+
+            if partner.company_name or partner.parent_id:
+                if not name and partner.type in ['invoice', 'delivery', 'other']:
+                    name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
+                if not partner.is_company:
+                    name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+            if self._context.get('show_address_only'):
+                name = partner._display_address(without_company=True)
+            if self._context.get('show_address'):
+                name = name + "\n" + partner._display_address(without_company=True)
+            name = name.replace('\n\n', '\n')
+            name = name.replace('\n\n', '\n')
+            if self._context.get('show_email') and partner.email:
+                name = "%s <%s>" % (name, partner.email)
+            if self._context.get('html_format'):
+                name = name.replace('\n', '<br/>')
+            res.append((partner.id, name))
+        return res
