cf2dcbb66fc3341b84c48d3cbfd5eb985a84822d
CrazyBolillo <random@email.com>
2020-04-30 13:26:24 -0500

    Removed demo data

--- __manifest__.py
+++ __manifest__.py
@@ -20,10 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    # cdfi_invoice is used for a specific scenario. If you don't use the module
-    # just leave the lines commented
     'depends': ['base', 'contacts', 'account_invoicing', 'l10n_mx'
-    # 'cdfi_invoice'
     ],
 
     # always loaded
@@ -32,8 +29,6 @@
         'views/res_partner_view.xml',
         'views/account_invoice_view.xml',
         'views/account_payment_view.xml',
-        'data/res_partner_data.xml',
-        # 'views/cdfi_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
